2e39954e7d3d400fdb6a17fcf4f31a76ec80a2d0
Elena Anisimova <el.n.anisimova@yandex.ru>
2020-05-01 23:26:49 +0700

    Init api with slim

--- api/public/index.php
+++ api/public/index.php
@@ -1,3 +1,20 @@
 <?php
+declare(strict_types=1);
 
-echo "Hello word";
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = AppFactory::create();
+
+// TODO add env and get displayErrorDetails value from env
+$app->addErrorMiddleware(true, true, true);
+
+$app->get('/', static function (Request $request, Response $response, array $args) {
+    $response->getBody()->write(json_encode(['hello' => 'world']));
+    return $response->withHeader('Content-type', 'application-json');
+});
+
+$app->run();
