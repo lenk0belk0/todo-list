c9d40a54815b994ccf5f3e20d59042db69fa0b2a
Elena Anisimova <el.n.anisimova@yandex.ru>
2020-05-02 18:47:37 +0700

    Add unit test

---
+++ api/tests/unit/Service/Http/JsonResponseTest.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types=1);
+
+use App\Service\Http\JsonResponse;
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ResponseInterface;
+
+class JsonResponseTest extends TestCase
+{
+    public function testReturnResponseInstance(): void
+    {
+        $response = new JsonResponse(null);
+
+        $this->assertInstanceOf(ResponseInterface::class, $response);
+        $this->assertSame(200, $response->getStatusCode());
+        $this->assertSame(['application-json'], $response->getHeader('Content-type'));
+    }
+
+    public function testReturnValidJson(): void
+    {
+        $response = new JsonResponse(['hello' => 'world']);
+
+        $this->assertSame('{"hello":"world"}', $response->getBody()->getContents());
+    }
+}
