c5ce632d1631081db1caabf8a93ad9c5ebcd8457
Elena Anisimova <el.n.anisimova@yandex.ru>
2020-05-02 16:09:40 +0700

    Add php di

--- api/public/index.php
+++ api/public/index.php
@@ -1,16 +1,16 @@
 <?php
 declare(strict_types=1);
 
-use App\Controller\HomeController;
-use Slim\Factory\AppFactory;
-
 require __DIR__ . '/../vendor/autoload.php';
 
-$app = AppFactory::create();
+$builder = new \DI\ContainerBuilder();
+$container = $builder->build();
+
+$app = \Slim\Factory\AppFactory::createFromContainer($container);
 
 // TODO add env and get displayErrorDetails value from env
 $app->addErrorMiddleware(true, true, true);
 
-$app->get('/', HomeController::class);
+$app->get('/', \App\Controller\HomeController::class);
 
 $app->run();
