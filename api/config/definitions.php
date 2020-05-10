3dc555c080edcaabf0fa0bc60d4816fc27cdd071
Elena Anisimova <el.n.anisimova@yandex.ru>
2020-05-10 23:47:46 +0700

    Add config for dev env in container

--- api/config/definitions.php
+++ api/config/definitions.php
@@ -1,4 +1,11 @@
 <?php
 declare(strict_types=1);
 
-return [];
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
+$dotenv->load();
+
+return [
+    'config' => [
+        'debug' => (bool) getenv('APP_ENV_DEV')
+    ],
+];
--- api/config/middleware.php
+++ api/config/middleware.php
@@ -5,5 +5,5 @@ use Psr\Container\ContainerInterface;
 use Slim\App;
 
 return static function(App $app, ContainerInterface $container) {
-    $app->addErrorMiddleware(true, true, true);
+    $app->addErrorMiddleware($container->get('config')['debug'], true, true);
 };
