8930eafb005bd7b61b8c696fb33a70f3abd184b3
Elena Anisimova <el.n.anisimova@yandex.ru>
2020-05-11 00:05:14 +0700

    Add doctrine entity manager to container

--- api/config/definitions.php
+++ api/config/definitions.php
@@ -1,6 +1,10 @@
 <?php
 declare(strict_types=1);
 
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Tools\Setup;
+
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
 $dotenv->load();
 
@@ -8,4 +12,28 @@ return [
     'config' => [
         'debug' => (bool) getenv('APP_ENV_DEV')
     ],
+    'EntityManager' => static function (DI\Container $container) {
+        $isDevMode = $container->get('config')['debug'];
+        $proxyDir = null;
+        $cache = null;
+        $useSimpleAnnotationReader = false;
+        $config = Setup::createAnnotationMetadataConfiguration(
+            [ __DIR__ . '/../src' ],
+            $isDevMode,
+            $proxyDir,
+            $cache,
+            $useSimpleAnnotationReader
+        );
+
+        // TODO change to postgresql
+        $conn = array(
+            'driver' => 'pdo_sqlite',
+            'path' => __DIR__ . '/../var/db.sqlite',
+
+        );
+
+        return EntityManager::create($conn, $config);
+    },
+    // TODO add logger
+    EntityManagerInterface::class => DI\get('EntityManager')
 ];
